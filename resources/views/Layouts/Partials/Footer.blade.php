c6f33e6af9fde595c202c7e4a2c466a6ba242882
nazrielalfarizi <nazriela13@gmail.com>
2022-10-19 08:29:33 +0700

    first commit

---
+++ resources/views/Layouts/Partials/Footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="footer-left">
+        &copy; 2022 <div class="bullet"></div><a href="https://mimhgogourung.sch.id/"> SMKN 4 Bandung</a> All Right Reserved.
+    </div>
+    <div class="footer-right">
+
+    </div>
+</footer>
